d9000c52f20b110cd3e735ba5020b997efe801ed
ZohaibVM <m.zohaib@visiomate.com>
2022-11-04 16:55:35 +0500

    azure function connected with frontend

---
+++ api/dummy-func/index.js
@@ -0,0 +1,7 @@
+module.exports = async function (context, req, _) {
+  console.log({ context, req, _ });
+  context.res = {
+    status: 200,
+    body: { message: 'This is an achievement' },
+  };
+};
--- components/AllForms/AllForms.js
+++ components/AllForms/AllForms.js
@@ -1,15 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import { deleteForm } from "../../store/formSlice";
 import Dropdown from "../Dropdown/Dropdown";
 
-const AllForms = () => {
+export default function AllForms() {
+  const [message, setMessage] = useState("");
   const { push } = useRouter();
   // const forms = useSelector((state) => state.allForms.forms);
+  useEffect(() => {
+    const apiCall = () => {
+      const res = fetch("http://localhost:7071/api/dummy-func")
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          setMessage(res?.message);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      // const messages = res.json();
+      // console.log({ messages });
+    };
+    apiCall();
+  }, []);
 
   return (
     <section className="all-forms">
+      <div>{message}</div>
       <div className="all-forms-wrapper">
         <div className="all-forms-single" onClick={() => push("/CreateForm")}>
           <i className="fa fa-2x fa-fw fa-file-o"></i>
@@ -26,6 +44,4 @@ const AllForms = () => {
       </div>
     </section>
   );
-};
-
-export default AllForms;
+}
