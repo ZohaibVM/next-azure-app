3654f020995bb7559afa8da68441957388e58b74
ZohaibVM <m.zohaib@visiomate.com>
2022-11-17 15:04:44 +0500

    logged envs in getForms

--- api/getForms/index.js
+++ api/getForms/index.js
@@ -4,6 +4,8 @@ module.exports = async function (context, req, _) {
   try {
     // const list = await blobService.getBlobs();
     const formsData = await blobService.downloadBlobData();
+    context.log("FIRST ENV::::", process.env.AZURE_STORAGE_CONNECTION_STRING);
+    context.log("SECOND ENV::::", process.env.AZURE_STORAGE_ACCOUNT_NAME);
 
     context.res = {
       status: 200,
