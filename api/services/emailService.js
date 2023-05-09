a7d4a449fea4297f373d5e0deb2a2041515bdae4
UsamaVM <87519296+UsamaVM@users.noreply.github.com>
2023-05-09 20:36:33 +0500

    Reverted mail server port change

--- api/services/emailService.js
+++ api/services/emailService.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: 465,
+  port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER, // generated ethereal user
